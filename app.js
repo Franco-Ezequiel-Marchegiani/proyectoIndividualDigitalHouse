a03fbd0976e8b39611366fe95b72347cc10d1acf
Franco Ezequiel <eze.cavs@hotmail.com>
2021-08-13 16:50:46 -0300

    Implementé un formulario detallado, tanto login + registro, sumado a vincular archivos HTML con Express

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ app.listen(3000, ()=>{
 app.use(express.static(path.join(__dirname, "./public")));
 
 
-app.get('/home', (req,res) =>{
-    res.sendFile(path.resolve(__dirname, "./views/home.html"))
+app.get("/home", (req,res) =>{
+    res.sendFile(path.join(__dirname, "./views/home.html"))
+})
+app.get("/register", (req,res) =>{
+    res.sendFile(path.join(__dirname, "./views/register.html"))
+})
+app.get("/login", (req,res) =>{
+    res.sendFile(path.join(__dirname, "./views/login.html"))
 })
